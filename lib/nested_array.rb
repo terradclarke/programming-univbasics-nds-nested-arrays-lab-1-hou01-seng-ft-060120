b4ee3b9916aa0d8fab0221d546121068be517fa0
Terranie Sims <terranie67@gmail.com>
2020-04-29 14:53:57 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,7 +31,7 @@ def sorted_matrix
     ]
 end
 
-def matrix_lookup(matrix, 1, 1)
+def matrix_lookup(matrix, row, collum)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
   
@@ -40,7 +40,7 @@ def matrix_lookup(matrix, 1, 1)
     ["Asparagus","Avocadoes","Grapes","Potatoes","Strawberries"   ]
     ]
     
-    produce_storage_room[1][1]
+    produce_storage_room[0][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
